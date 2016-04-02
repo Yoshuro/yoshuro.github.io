3b99c51ba327abffe6203b0c7feaeec69cb0ac91
Yoshuro <yoshuro@hotmail.com>
2016-04-02 20:59:42 +0200

    tempo

--- js/core.js
+++ js/core.js
@@ -14,8 +14,8 @@ var main = function () {
         var currentSlide=$('.active-slide');
         var nextSlide=currentSlide.next();
         if(nextSlide.length===0) nextSlide=$('.slide').first();
-        currentSlide.fadeOut(100).removeClass('active-slide');
-        nextSlide.fadeIn(100).addClass('active-slide'); 
+        currentSlide.fadeOut(120).removeClass('active-slide');
+        nextSlide.fadeIn(120).addClass('active-slide'); 
         var currentDot = $('.active-dot');
         var nextDot = currentDot.next();
         if(nextDot.length===0) nextDot = $('.dot').first();
@@ -26,8 +26,8 @@ var main = function () {
         var currentSlide=$('.active-slide');
         var prevSlide=currentSlide.prev();
         if(prevSlide.length===0) prevSlide=$('.slide').last();
-        currentSlide.fadeOut(100).removeClass('active-slide');
-        prevSlide.fadeIn(100).addClass('active-slide'); 
+        currentSlide.fadeOut(120).removeClass('active-slide');
+        prevSlide.fadeIn(120).addClass('active-slide'); 
         var currentDot = $('.active-dot');
         var prevDot = currentDot.prev();
         if(prevDot.length===0) prevDot = $('.dot').last();
